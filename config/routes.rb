dbc0e4db3ec6080b8de90548a269f616f8640928
mteregh <mtguerra@miuandes.cl>
2026-04-16 13:47:54 -0400

    Define routes with index, show for all resources, set root to owners#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,9 @@
 Rails.application.routes.draw do
-  get "appointments/index"
-  get "appointments/show"
-  get "vets/index"
-  get "vets/show"
-  get "pets/index"
-  get "pets/show"
-  get "owners/index"
-  get "owners/show"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "owners#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :owners, only: [:index, :show]
+  resources :pets, only: [:index, :show]
+  resources :vets, only: [:index, :show]
+  resources :appointments, only: [:index, :show]
+  
 end
