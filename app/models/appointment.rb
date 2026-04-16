f61f88618a0f5f3fccba853a59e50dcf990370d6
mteregh <mtguerra@miuandes.cl>
2026-04-16 19:27:14 -0400

    Format dates, statuses and apply Bootstrap

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,12 @@ class Appointment < ApplicationRecord
   belongs_to :pet
   belongs_to :vet
   has_many :treatments
+
+  enum :status, {
+    scheduled: 0,
+    in_progress: 1,
+    completed: 2,
+    cancelled: 3
+  }
+  
 end
